30240f1ef7a35155d0d128dc19b8e043adeb840d
Kaden Thomas <thomas.kaden4@outlook.com>
2020-06-07 00:30:43 -0700

    Add google analytics code

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,17 @@ import Head from "next/head";
 export function Header({ title }) {
   return (
     <Head>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168784785-1"></script>
+      <script>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-168784785-1');
+`}
+      </script>
       <title>{title + " - Humanality"}</title>
       <link rel="icon" href="/logo64.png" />
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
